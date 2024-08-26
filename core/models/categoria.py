6877c0eceb69cbc2af75f189c244b1c0864d1ee5
BeatrizVenturi <venturibeatriz329@gmail.com>
2024-08-26 15:07:30 -0300

    Adiciona relacionamento de Livro com Categoria e Editora.

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -1,8 +1,10 @@
 from django.db import models
+from .editora import Editora
 
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
 
     def __str__(self):
         return self.descricao
